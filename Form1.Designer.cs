63e6de09d6f7b49a69a4d8e6286dea88bbc2d125
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2024-01-02 10:16:39 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace DateTime_Kullanımı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblMon = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblDow = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.lblDoy = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.lblTod = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.lblMsec = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.lblTicks = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SpringGreen;
+            this.button1.Location = new System.Drawing.Point(23, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Date";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(132, 37);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 13);
+            this.lblDate.TabIndex = 1;
+            // 
+            // lblMon
+            // 
+            this.lblMon.AutoSize = true;
+            this.lblMon.Location = new System.Drawing.Point(132, 88);
+            this.lblMon.Name = "lblMon";
+            this.lblMon.Size = new System.Drawing.Size(0, 13);
+            this.lblMon.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SpringGreen;
+            this.button2.Location = new System.Drawing.Point(23, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Month";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Location = new System.Drawing.Point(132, 141);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(0, 13);
+            this.lblDay.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SpringGreen;
+            this.button3.Location = new System.Drawing.Point(23, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Day";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(132, 197);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(0, 13);
+            this.lblYear.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SpringGreen;
+            this.button4.Location = new System.Drawing.Point(23, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Year";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lblDow
+            // 
+            this.lblDow.AutoSize = true;
+            this.lblDow.Location = new System.Drawing.Point(132, 253);
+            this.lblDow.Name = "lblDow";
+            this.lblDow.Size = new System.Drawing.Size(0, 13);
+            this.lblDow.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.SpringGreen;
+            this.button5.Location = new System.Drawing.Point(23, 249);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "DayOfWeek";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // lblDoy
+            // 
+            this.lblDoy.AutoSize = true;
+            this.lblDoy.Location = new System.Drawing.Point(132, 309);
+            this.lblDoy.Name = "lblDoy";
+            this.lblDoy.Size = new System.Drawing.Size(0, 13);
+            this.lblDoy.TabIndex = 11;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.SpringGreen;
+            this.button6.Location = new System.Drawing.Point(23, 305);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "DayOfYear";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // lblTod
+            // 
+            this.lblTod.AutoSize = true;
+            this.lblTod.Location = new System.Drawing.Point(132, 357);
+            this.lblTod.Name = "lblTod";
+            this.lblTod.Size = new System.Drawing.Size(0, 13);
+            this.lblTod.TabIndex = 13;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.SpringGreen;
+            this.button7.Location = new System.Drawing.Point(23, 353);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "TimeOfDay";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Location = new System.Drawing.Point(132, 405);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(0, 13);
+            this.lblHour.TabIndex = 15;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.SpringGreen;
+            this.button8.Location = new System.Drawing.Point(23, 401);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Hour";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Location = new System.Drawing.Point(436, 36);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(0, 13);
+            this.lblMinute.TabIndex = 17;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.SpringGreen;
+            this.button9.Location = new System.Drawing.Point(327, 32);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "Minute";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Location = new System.Drawing.Point(436, 87);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(0, 13);
+            this.lblSec.TabIndex = 19;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.SpringGreen;
+            this.button10.Location = new System.Drawing.Point(327, 83);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "Second";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // lblMsec
+            // 
+            this.lblMsec.AutoSize = true;
+            this.lblMsec.Location = new System.Drawing.Point(436, 135);
+            this.lblMsec.Name = "lblMsec";
+            this.lblMsec.Size = new System.Drawing.Size(0, 13);
+            this.lblMsec.TabIndex = 21;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.SpringGreen;
+            this.button11.Location = new System.Drawing.Point(327, 131);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 20;
+            this.button11.Text = "MilliSecond";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // lblTicks
+            // 
+            this.lblTicks.AutoSize = true;
+            this.lblTicks.Location = new System.Drawing.Point(436, 191);
+            this.lblTicks.Name = "lblTicks";
+            this.lblTicks.Size = new System.Drawing.Size(0, 13);
+            this.lblTicks.TabIndex = 23;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.SpringGreen;
+            this.button12.Location = new System.Drawing.Point(327, 187);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 22;
+            this.button12.Text = "Ticks";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.lblTicks);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.lblMsec);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.lblMinute);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.lblTod);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.lblDoy);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.lblDow);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblMon);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblMon;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblDow;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label lblDoy;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label lblTod;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label lblMsec;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label lblTicks;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTime_Kullanımı
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DateTime bugun = DateTime.Now;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblDate.Text = bugun.Date.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblMon.Text = bugun.Month.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lblDay.Text = bugun.Day.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lblYear.Text = bugun.Year.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            lblDow.Text = bugun.DayOfWeek.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            lblDoy.Text = bugun.DayOfYear.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            lblTod.Text = bugun.TimeOfDay.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            lblHour.Text = bugun.Hour.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            lblMinute.Text = bugun.Minute.ToString();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            lblSec.Text = bugun.Second.ToString();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            lblMsec.Text = bugun.Millisecond.ToString();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            lblTicks.Text = bugun.Ticks.ToString();
+        }
+    }
+}
